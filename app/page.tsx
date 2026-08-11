e62ca307a1c8560650b1a4753b45a3caa1bb5d59
JGOJLG <jen@jgohire.com>
2026-08-11 10:50:59 -0400

    Add free survival guide to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import CredibilityStrip from "@/components/CredibilityStrip";
 import HowCanIHelp from "@/components/HowCanIHelp";
 import Services from "@/components/Services";
+import FreeSurvivalGuide from "@/components/FreeSurvivalGuide";
 import RecruiterAdvantage from "@/components/RecruiterAdvantage";
 import FinalCTA from "@/components/FinalCTA";
 import LinkedInGuide from "@/components/LinkedInGuide";
@@ -16,6 +17,7 @@ export default function HomePage() {
       <CredibilityStrip />
       <HowCanIHelp />
       <Services />
+      <FreeSurvivalGuide />
       <LinkedInGuide />
       <RecruiterAdvantage />
       <FinalCTA />
