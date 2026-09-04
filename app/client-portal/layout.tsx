21b13d2b216ca8ae6d89b99ce332ef29567ef115
JGOJLG <jen@jgohire.com>
2026-09-03 22:05:15 -0400

    Use route-aware portal header

--- app/client-portal/layout.tsx
+++ app/client-portal/layout.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import "./portal.css";
+import PortalHeader from "./PortalHeader";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -33,21 +33,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           }
         }
       `}</style>
-      <header className="cp-header">
-        <div className="cp-shell cp-header-inner">
-          <Link href="/client-portal" className="cp-brand">
-            <strong>JGO HIRE</strong>
-            <span>Client Portal</span>
-          </Link>
-          <nav className="cp-nav" aria-label="Client portal navigation">
-            <Link href="/client-portal">Home</Link>
-            <Link href="/client-portal/jobs">Job Tracker</Link>
-            <Link href="/client-portal/files">Documents</Link>
-            <Link href="/client-portal/resources">Resources</Link>
-            <Link href="/auth/signout">Log out</Link>
-          </nav>
-        </div>
-      </header>
+      <PortalHeader />
       {children}
     </div>
   );
